62ab454ed50380e2d040c187b19f80ada5d1e921
Rabia Alhaffar <47897019+Rabios@users.noreply.github.com>
2020-02-06 19:06:15 +0200

    Add files via upload

---
+++ graphics_01.js
@@ -0,0 +1,18 @@
+CreateCanvas(350,400,"3px black solid");
+Initialize();
+SetDrawingMode("fill");
+SetFont("20px arial");
+var x = 0,y = 0,h = 50,w = 50;
+for(i = 0;i < 7;i++)
+{
+DrawRect(x,y,h,w,"red");
+DrawRect(x += 50,y,h,w,"orange");
+DrawRect(x += 50,y,h,w,"yellow");
+DrawRect(x += 50,y,h,w,"green");
+DrawRect(x += 50,y,h,w,"blue");
+DrawRect(x += 50,y,h,w,"pink");
+DrawRect(x += 50,y,h,w,"purple");
+y += 50;
+x = 0;
+}
+DrawText(35,375,"Made With Cake Game Engine!!!","black","center");
