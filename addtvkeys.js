ced9b5b78fda340092bd77ec6aeb0a0c4e941520
Rabia Alhaffar <47897019+Rabios@users.noreply.github.com>
2020-02-06 20:24:09 +0200

    Add files via upload

---
+++ addtvkeys.js
@@ -0,0 +1,19 @@
+Mousetrap.addKeycodes({
+    516: 'channel-up',
+	517: 'channel-down',
+	588: 'f0',
+	589: 'f1',
+	590: 'f2',
+	591: 'f3',
+	642: 'aspect',
+	615: 'info',
+	651: 'lastview',
+	250: 'play',
+	19: 'pause',
+	178: 'stop',
+	603: 'record',
+	176: 'skip-to-next',
+	177: 'skip-to-previous',
+	228: 'next',
+	227: 'previous'
+});
