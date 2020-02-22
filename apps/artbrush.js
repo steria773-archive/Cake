a39716dcf00074a2e2e8ce5a30748da14e4c0426
Rabia Alhaffar <47897019+Rabios@users.noreply.github.com>
2020-02-22 16:24:37 +0200

    Add files via upload

--- apps/artbrush.js
+++ apps/artbrush.js
@@ -4,7 +4,7 @@ var drawmode = "fill";
 var brushsize = 5;
 var alpha = 1;
 var brushishidden = false;
-alert("Welcome To Artbrush App!!!\nMove The Mouse To Paint With Brush\nClick Canvas To Toggle Full Screen\nPress C Key If You Want To Change Brush Color\nPress S Key If You Want To Change Brush Size\nPress T Key If You Want To Change Brush Type\nPress H Key To Hide Brush\nPress B Key To Show Brush\nPress E Key To Take Screenshot\nPress M Key To Set Brush Painting Mode\nPress Q Key To Show This Info Again\nPowered By Cake Game Engine!!!");
+alert("Welcome To Artbrush App!!!\nMove The Mouse To Paint With Brush\nClick Canvas To Toggle Full Screen\nPress O Key To Set Background Color\nPress G Key To Set Grid\nPress C Key If You Want To Change Brush Color\nPress S Key If You Want To Change Brush Size\nPress T Key If You Want To Change Brush Type\nPress H Key To Hide Brush\nPress B Key To Show Brush\nPress E Key To Take Screenshot\nPress M Key To Set Brush Painting Mode\nPress Q Key To Show This Info Again\nPowered By Cake Game Engine!!!");
 CreateCanvas(window.innerWidth,window.innerHeight,"white");
 Initialize();
 SetDrawingMode(drawmode);
@@ -30,8 +30,18 @@ document.addEventListener("keydown",(e) =>
     }
     if(e.key == "b") brushishidden = false;
     if(e.key == "h") brushishidden = true;
+    if(e.key == "o") 
+    {
+        var bgcolor = prompt("Enter Background Color: ","white").toString();
+        SetCanvasBackgroundColor(bgcolor);
+    }
+    if(e.key == "g")
+    { 
+       var gridsize = prompt("Before Drawing Grid,Please Enter Grid Size: ",20);
+       DrawGrid(gridsize,"black",1);
+    }
     if(e.key == "e") Screenshot();
-    if(e.key == "q") alert("Welcome To Artbrush App!!!\nMove The Mouse To Paint With Brush\nPress C Key If You Want To Change Brush Color\nPress S Key If You Want To Change Brush Size\nPress T Key If You Want To Change Brush Type\nPress H Key To Hide Brush\nPress Ctrl Key To Show Brush\nPress M Key To Set Brush Painting Mode\nPowered By Cake Game Engine!!!");
+    if(e.key == "q") alert("Welcome To Artbrush App!!!\nMove The Mouse To Paint With Brush\nClick Canvas To Toggle Full Screen\nPress O Key To Set Background Color\nPress G Key To Set Grid\nPress C Key If You Want To Change Brush Color\nPress S Key If You Want To Change Brush Size\nPress T Key If You Want To Change Brush Type\nPress H Key To Hide Brush\nPress B Key To Show Brush\nPress E Key To Take Screenshot\nPress M Key To Set Brush Painting Mode\nPress Q Key To Show This Info Again\nPowered By Cake Game Engine!!!");
     if(e.key == "c") brushcolor = prompt("Enter Brush Color: ","black").toString();
     if(e.key == "s") brushsize = prompt("Enter Brush Size: ",1);
     if(e.key == "t") brushtype = prompt("Brush Types List:\n1. pencil\n2. squares\n3. arc\nEnter Brush Type(Name Or Number): ","black").toString();
