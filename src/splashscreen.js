808180f9bac1b387f3890c60a4756b9b4e0c0b9d
Rabia Alhaffar <47897019+Rabios@users.noreply.github.com>
2020-03-07 10:47:42 +0200

    Add files via upload

--- src/splashscreen.js
+++ src/splashscreen.js
@@ -1,6 +1,6 @@
 //Created By Rabia Alhaffar In 26/December/2019
 //For Splash Screen Creation
-var SetSplashscreen = (src,time,level) =>
+var SetSplashscreen = (src,level,time) =>
 {
 cakecanvas.style.backgroundImage = `url(${src.toString()})`;
 setTimeout(() => { cakecanvas.style.backgroundImage = "none"; level.Start(); },time); 
