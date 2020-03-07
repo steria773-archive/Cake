bd2ac8142ae3ce7ce4587052fca9d1031b5e5eb1
Rabia Alhaffar <47897019+Rabios@users.noreply.github.com>
2020-03-07 10:46:48 +0200

    Add files via upload

--- apps/cake.js
+++ apps/cake.js
@@ -2752,7 +2752,7 @@ var SetLoadingLevel = (loading_level, level, time) =>
 //Module: Splashscreen
 //Created By Rabia Alhaffar In 26/December/2019
 //For Splash Screen Creation
-var SetSplashscreen = (src, time, level) =>
+var SetSplashscreen = (src, level, time) =>
 {
 	cakecanvas.style.backgroundImage = `url(${src.toString()})`;
 	setTimeout(() =>
