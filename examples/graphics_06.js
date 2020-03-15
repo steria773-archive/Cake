cba8d736eb915b80bc405ea20c9d32fa849c8013
Rabia Alhaffar <47897019+Rabios@users.noreply.github.com>
2020-03-15 18:47:13 -0400

    Add files via upload

---
+++ examples/graphics_06.js
@@ -0,0 +1,7 @@
+//Created By Rabia Alhaffar In 15/March/2020
+//Welcome To WebGL!!!
+CreateCanvas(600,600,"black");
+InitializeWebGL(0);
+setInterval(() => {
+    WebGLClearCanvas(Math.random(),Math.random(),Math.random(),1.0);
+},500);
