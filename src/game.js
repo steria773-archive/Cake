0ba506839e23cb6110557cd43291fbbc67993756
Rabia Alhaffar <steria773@gmail.com>
2020-03-17 18:35:09 -0400

    Add files via upload

--- src/game.js
+++ src/game.js
@@ -7,7 +7,7 @@ var CrashGame = () => { while(true) console.log(0); };
 var ViewSourceCode = (index) => window.open(document.scripts[index].src);
 var ViewGameSourceCode = () => window.open(document.scripts[2].src);
 var StartProcess = (dir) => window.open("file:///" + dir);
-var UpdateCake = () => Import("https://cdn.jsdelivr.net/gh/Rabios/Cake/build/cake.js");
+var UpdateCake = () => Import("https://cdn.jsdelivr.net/gh/Cake-Engine/Cake@master/build/cake.js");
 var Destroy = (component) =>
 {
     if(!component.destroyed) component.Destroy();
