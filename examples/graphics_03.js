19cd7939306bbd8012de2b2352ad061c6ef6f059
Rabia Alhaffar <steria773@gmail.com>
2020-03-23 20:00:51 -0400

    Add files via upload

--- examples/graphics_03.js
+++ examples/graphics_03.js
@@ -1,13 +1,13 @@
-    var Random_X = Math.floor(Math.random() * 400);
-    var Random_Y = Math.floor(Math.random() * 400);
-    CreateCanvas(400,400,u,"black 3px solid");
-    Initialize();
-    SetDrawingMode("custom");
-    var Scene_1 = new Level(function()
-    {  
+var Random_X = Math.floor(Math.random() * 400);
+var Random_Y = Math.floor(Math.random() * 400);
+CreateCanvas(400,400,u,"black 3px solid");
+Initialize();
+SetDrawingMode("custom");
+var Scene_1 = new Level(function()
+{
     DrawSquare(Random_X,Random_Y,50,RandomColor());
     Random_X = Math.floor(Math.random() * 400);
     Random_Y = Math.floor(Math.random() * 400);
     DrawFPS(Scene_1);
-    },120);
-    Scene_1.Start();
+},120);
+Scene_1.Start();
--- examples/graphics_06.js
+++ examples/graphics_06.js
@@ -1,7 +1,7 @@
 //Created By Rabia Alhaffar In 15/March/2020
 //Welcome To WebGL!!!
 CreateCanvas(600,600,"black");
-InitializeWebGL(0);
+InitializeWebGL();
 setInterval(() => {
     WebGLClearCanvas(Math.random(),Math.random(),Math.random(),1);
 },500);
