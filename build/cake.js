a6c158a7b6b4bf594bc49a6619f444587edfd40a
Rabia Alhaffar <steria773@gmail.com>
2020-03-24 13:17:14 -0400

    Add files via upload

--- build/cake.js
+++ build/cake.js
@@ -4698,3 +4698,33 @@ var BB_GetFont = () => blackberry.system.getFontInfo();
 var BB_GetCurrentTimezone = () => blackberry.system.getCurrentTimezone();
 var BB_SetWallpaper = (image_location) => blackberry.system.setWallpaper(image_location);
 var BB_Rotate = (screen_orientation) => blackberry.app.rotate(screen_orientation);
+
+//Module: Shaders For Basic Usages
+//Created By Rabia Alhaffar In 24/March/2020
+//All Shaders
+var default_position_shader = `
+//Don't Treat It As Error,lightgl.js Run It Anyway
+//When You Add It,Make Sure Your Mesh Has Normals Enabled
+//Can Be Using { normals: true }
+void main() {
+    gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+}`;
+
+var normals_default_position = `
+//Don't Treat It As Error,lightgl.js Run It Anyway
+//When You Add It,Make Sure Your Mesh Has Normals Enabled
+//Can Be Using { normals: true }
+varying vec3 normal;
+void main() {
+    normal = gl_NormalMatrix * gl_Normal; 
+    gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+}`;
+
+var normals_fragment = `
+//Don't Treat It As Error,lightgl.js Run It Anyway
+//When You Add It,Make Sure Your Mesh Has Normals Enabled
+//Can Be Using { normals: true }
+varying vec3 normal;
+void main() {
+    gl_FragColor = vec4(normalize(normal) * 0.5 + 0.5, 1.0);
+}`;
